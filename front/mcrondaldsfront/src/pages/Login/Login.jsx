28dc513f6e10fdd5e062e61c3f6ee7b37ee8b1da
Erixon Castillo Gabriel <u20228504@utp.edu.pe>
2025-12-02 18:38:45 -0500

    Fix(despliegue): Se corrigio un error al desplegar el front

--- front/mcrondaldsfront/src/pages/Login/Login.jsx
+++ front/mcrondaldsfront/src/pages/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({ onLogin }) => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:8080/api/auth/login", {
+            const response = await fetch("https://mc-ronald-s-1.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
