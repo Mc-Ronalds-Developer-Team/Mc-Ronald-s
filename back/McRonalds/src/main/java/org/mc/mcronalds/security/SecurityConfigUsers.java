44619be33f13350d557e4685a934656298c262a6
LuisVC <U22241090@utp.edu.pe>
2025-12-11 09:29:06 -0500

    feat: add initial spring security configuration for api endpoints.

--- back/McRonalds/src/main/java/org/mc/mcronalds/security/SecurityConfigUsers.java
+++ back/McRonalds/src/main/java/org/mc/mcronalds/security/SecurityConfigUsers.java
@@ -44,7 +44,7 @@ public class SecurityConfigUsers {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // IMPORTANTE: Aquí pon la URL real de tu Frontend en Render
-        // Si quieres probar rápido (pero inseguro) usa "*" en lugar de la URL
+        // configuration.setAllowedOrigins(List.of("https://mc-ronald-s.onrender.com"));
         configuration.setAllowedOrigins(List.of("https://mc-ronald-s-frontend.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
